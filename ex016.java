551173c8157d1c54f20bbea72586074b0fda2e7b
anna-xlv <Annal.guido1a@gmail.com>
2025-04-02 07:15:29 -0300

    Leia o nome do usuário e escreva o nome dele na tela.

---
+++ ex016.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+16
+
+*******************************************************************************/
+import java.util.Scanner;
+public class ex016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nome;
+
+        System.out.println("digite o nome do usuario: ");
+        nome = sc.nextLine();
+
+        for (int i = 0; i <= 10; i++)
+            System.out.println( nome +"!!!:b");
+
+    }
+}
