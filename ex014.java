eb9539e4ab081f08f9e73505c434a31aa8e5ec3f
anna-xlv <Annal.guido1a@gmail.com>
2025-04-02 07:14:27 -0300

    Escreva um programa que exiba 10 vezes uma mensagem.

---
+++ ex014.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+14
+
+*******************************************************************************/
+ public class ex014 {
+     public static void main(String[] args) {
+         int n;
+
+ 
+         for (int i = 0; i <= 10; i++)
+             System.out.println("Eu gosto de estudar Algoritmos!:D");
+ 
+     }
+ }
