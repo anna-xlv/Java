65d20332a075caee55fdb5e7cd4c11e85da3419d
anna-xlv <Annal.guido1a@gmail.com>
2025-04-07 15:06:56 -0300

    aividade

---
+++ ex34.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+atividade 34
+
+*******************************************************************************/
+public class ex34 {
+    public static void main(String[] args) {
+        int contador = 0;
+        
+        do{
+            System.out.println(contador);
+            contador++;
+        } while (contador <= 5);
+        
+    }
+}
