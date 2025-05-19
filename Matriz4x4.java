381e72c4e4799db75373ee3b54ea3a65e8254b0e
anna-xlv <Annal.guido1a@gmail.com>
2025-05-19 11:17:52 -0300

    avividade 14/05

---
+++ Matriz4x4.java
@@ -0,0 +1,20 @@
+
+public class Matriz4x4 {
+    public static void main(String[] args) {
+
+        int[][] matriz = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+
+        System.out.println("Matriz 4x4");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
